49da189afb9931d5c408043be39d4b8f20d15fa7
anco <akidesign303@gmail.com>
2025-01-08 13:08:01 +0900

    確認用consoleを消去

--- js/script.js
+++ js/script.js
@@ -3,37 +3,31 @@ function highlight (ele, color) {
 }
 
 $('.button p').mouseleave(function(){
-    console.log('マウスはなれた');
     highlight( '.main-container p', '#ebe9e9');
 }
 )
 
 $('.local').mouseover(function(){
-    console.log('マウスオーバー');
     highlight( '.1, .2, .3, .4, .5', '#af26a1');
 }
 )
 
 $('.rapid').mouseover(function(){
-    console.log('マウスオーバー');
     highlight( '.2, .3, .4, .5', '#2a1ffd');
 }
 )
 
 $('.semiExpress').mouseover(function(){
-    console.log('マウスオーバー');
     highlight( '.3, .4, .5', 'orange');
 }
 )
 
 $('.express').mouseover(function(){
-    console.log('マウスオーバー');
     highlight( '.4, .5', 'rgb(0, 186, 0)');
 }
 )
 
 $('.specialExpress').mouseover(function(){
-    console.log('マウスオーバー');
     highlight( '.5', '#ff0000');
 }
 )
